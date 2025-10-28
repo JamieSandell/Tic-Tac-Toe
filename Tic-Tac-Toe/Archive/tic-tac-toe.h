5c2cde3ae30223946c11a9533e4bd1b2003faca4
Jamie Sandell <jamie.sandell@outlook.com>
2025-10-28 07:16:00 +0000

    new structure

--- Tic-Tac-Toe/Archive/tic-tac-toe.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-// Services that the platform layer provides to the game layer:
-
-// Services that the game layer provides to the platform layer:
-void game_update_and_render(void);
---
+++ code/win32_ttt.c
@@ -0,0 +1,12 @@
+#include <windows.h>
+
+int CALLBACK
+WinMain (
+    HINSTANCE instance,
+    HINSTANCE previous_instance,
+    LPSTR command_line,
+    int show_command
+)
+{
+    return 0;
+}
